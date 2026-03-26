0764f9924760828b40142c2d037717934188cb26
mamadou-abass-diallo-sre <m.abassdiallo@gmail.con>
2026-03-26 16:11:47 +0000

    init-project

---
+++ src/main/java/sn/symmetry/cadoobi/CadoobiApplication.java
@@ -0,0 +1,13 @@
+package sn.symmetry.cadoobi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadoobiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadoobiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/symmetry/cadoobi/CadoobiApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.symmetry.cadoobi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadoobiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
